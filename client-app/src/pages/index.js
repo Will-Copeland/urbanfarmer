db33bdb6166a773a499eef4d7ae146b0038fbd09
Will-Copeland <will@localhost.localdomain>
2019-10-25 21:32:27 -0700

    firebase working, tslint installed, not setup

--- client-app/src/pages/index.js
+++ client-app/src/pages/index.js
@@ -1,11 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import * as firebase from "firebase";
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const firebaseConfig = {
+  apiKey: "AIzaSyBAkMD__bZ0zMsBkM8Qbag9Z0CiWMxq35Q",
+  authDomain: "urbanfarmer-c46e0.firebaseapp.com",
+  databaseURL: "https://urbanfarmer-c46e0.firebaseio.com",
+  projectId: "urbanfarmer-c46e0",
+  storageBucket: "urbanfarmer-c46e0.appspot.com",
+  messagingSenderId: "374488641283",
+  appId: "1:374488641283:web:60d0da10cea5b24d784336"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const IndexPage = () => {
+
+  React.useEffect(() => {
+    getDoc();
+  }, [])
+
+  return (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -16,6 +34,19 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+  )
+
+  function getDoc() {
+    firebase.firestore()
+    .collection("test")
+    .get()
+    .then(resp =>{
+      resp.forEach(doc => {
+        console.log(doc.data());
+        
+      })
+    })
+  }
+}
 
 export default IndexPage
